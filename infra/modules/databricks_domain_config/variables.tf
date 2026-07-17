6253bd9ee088aca23093f300bf7109aa48acc8e4
Roberto Borges <borgesjoy@gmail.com>
2026-07-17 09:38:41 -0300

    feat: refatoração da infraestrutura sales_domain e adicão de configuração dinâmica de módulos

---
+++ infra/modules/databricks_domain_config/variables.tf
@@ -0,0 +1,4 @@
+variable "azure_subscription_id" { type = string }
+variable "databricks_rg_name" { type = string }
+variable "databricks_workspace_name" { type = string }
+variable "databricks_domain_name" { type = string }
